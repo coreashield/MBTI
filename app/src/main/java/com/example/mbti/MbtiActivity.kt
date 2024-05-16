f8b8b698dd6d356af72d194a5222696ec58cb791
coreashield <120443729+coreashield@users.noreply.github.com>
2024-05-16 23:11:40 +0900

    Update MbtiActivity.kt

--- app/src/main/java/com/example/mbti/MbtiActivity.kt
+++ app/src/main/java/com/example/mbti/MbtiActivity.kt
@@ -1,99 +1,96 @@
 package com.example.mbti
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mbti.ui.theme.MBTITheme
 
-class MbtiActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MBTITheme {
-                val mbtiData = intent.getStringExtra("mbti_data") ?: ""
-                MainScreen(mbtiData)
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    MainScreen()
+
+                    Button(
+                        onClick = {
+                            finish()
+                        },
+                        modifier = Modifier
+                            .size(300.dp, 50.dp)
+                            .padding(top = 10.dp),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    {
+                        Text(text = "앱 종료", fontSize = 10.sp)
+                    }
+                }
             }
         }
     }
 }
 
-//@Preview(showBackground = true)
-@Composable
-fun MainScreen(mbtiData : String) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(start = 8.dp, end = 8.dp),
 
-        horizontalAlignment = Alignment.CenterHorizontally
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    MainScreen()
+}
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+
+    TopTextView("16가지 다양한 MBTI들")
+
+    MbtiButton(javaclass = MbtiActivity::class.java, "INFP")
+    MbtiButton(javaclass = MbtiActivity::class.java, "INFJ")
+    MbtiButton(javaclass = MbtiActivity::class.java, "ISFJ")
+}
+
+@Composable
+fun TopTextView(s: String) {
+    Text(text = s, fontSize = 20.sp)
+}
+
+@Composable
+fun MbtiButton(javaclass: Class<*>, str: String) {
+    val context = LocalContext.current as? Activity
+    Button(
+        onClick = {
+            val intent = Intent(context, javaclass)
+            intent.putExtra("mbti_data", str)
+            context?.startActivity(intent)
+        }, modifier = Modifier
+            .size(300.dp, 50.dp)
+            .padding(top = 10.dp),
+        shape = RoundedCornerShape(10.dp)
     ) {
-        Box {
-            Text(
-                modifier = Modifier
-                    .align(Alignment.TopCenter),
-                text = mbtiData,
-                fontSize = 35.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-        when(mbtiData){
-            "INFP" -> Column{
-                Painting(paintId = R.drawable.infp1)
-                Painting(paintId = R.drawable.infp2)
-            }
-            "ISFJ" -> Column{
-                Painting(paintId = R.drawable.isfj1)
-                Painting(paintId = R.drawable.isfj2)
-            }
-        }
-
-        Row {
-            val context = LocalContext.current as? Activity
-            Button(
-                onClick = { context?.finish() },
-                modifier = Modifier
-                    .width(205.dp)
-                    .height(105.dp)
-                    .padding(top = 40.dp, bottom = 10.dp)
-            ) {
-                Text(text = "이전 화면으로", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            }
-        }
+        Text(text = str)
     }
-}
-
-
-@Composable
-fun Painting(paintId: Int) {
-    Image(
-        painter = painterResource(id = paintId),
-        contentDescription = "",
-        modifier = Modifier
-            .fillMaxWidth()
-            .size(width = 10.dp, height = 300.dp)
-            .padding(top = 1.dp, bottom = 10.dp)
-    )
 }
